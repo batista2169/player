3d34939d51af6a0ad2a8144581768f39ce7359a3
batista2169 <71348110+batista2169@users.noreply.github.com>
2024-03-12 16:10:07 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ $(window).on('load', function () {
     $('#play-btn').on('click', function () {
         localStorage.setItem('m3u8-link', $('#m3u8-placeholder')[0].value);
         window.location.href = './player' + '?source=' + $('#m3u8-placeholder')[0].value;
-        jw.file = $('#m3u8-placeholder')[0].value;
+        mylink = $('#m3u8-placeholder')[0].value;
     });
 });
